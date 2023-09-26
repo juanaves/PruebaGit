e4b65faa46aa31e3e70600f7d0ff9c90fb070a1d
juan <juanaves@gmail.com>
2023-09-26 09:24:37 +0200

    He creado la clase empleado

---
+++ src/Empleado.java
@@ -0,0 +1,16 @@
+public class Empleado {
+    int id;
+    String nombre;
+
+    public Empleado(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
